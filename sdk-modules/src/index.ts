aa5f20a1313750001c791b8684d0850c58c14c52
MeetzhDing <3303652975@qq.com>
2022-12-09 13:00:30 +0800

    chore: export consoleConstants from sdk-module/index.ts

--- sdk-modules/src/index.ts
+++ sdk-modules/src/index.ts
@@ -6,6 +6,7 @@ export type {
   ISdkSearchPageProps,
 } from '@tencent/tea-sdk-cls-types';
 export type { CAPIRequest } from '@tencent/tea-sdk-runner/lib/modules/capi';
+export { constants as consoleConstants } from '@tencent/tea-sdk-runner/lib/modules/constants'
 export * from './initSdkRunner';
 export * from './SdkSearchPage';
 export * from './SdkDashboard';
